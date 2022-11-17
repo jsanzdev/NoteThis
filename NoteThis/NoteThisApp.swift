4bfbf7e88b8a2846a7beeeca52d938f84037d5cb
Jesus Sanz <jesus@jsanz.dev>
2022-11-17 22:38:18 +0100

    Initial Commit

---
+++ NoteThis/NoteThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteThisApp.swift
+//  NoteThis
+//
+//  Created by Jesus Sanz on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NoteThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
